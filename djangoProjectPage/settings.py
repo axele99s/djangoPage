a4d34342c340b62fa1ad1d092de3cc24cda61afb
Axel Echevarria <axelechev@gmail.com>
2024-10-24 02:38:36 -0300

    .

--- djangoProjectPage/settings.py
+++ djangoProjectPage/settings.py
@@ -122,8 +122,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'noticias/static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # O la ruta que prefieras
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
